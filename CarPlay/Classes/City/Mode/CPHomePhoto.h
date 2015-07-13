ff05689125fe960667587acfcec2297daa912234
公平价 <2234475139@qq.com>
2015-07-13 18:12:37 +0800

    City 界面封装相关类

---
+++ CarPlay/Classes/City/Mode/CPHomePhoto.h
@@ -0,0 +1,16 @@
+//
+//  CPHomePhoto.h
+//  CarPlay
+//
+//  Created by 公平价 on 15/7/13.
+//  Copyright (c) 2015年 gongpingjia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPHomePhoto : NSObject
+
+/** 缩略图 */
+@property (nonatomic, copy) NSString *thumbnail_pic;
+
+@end
