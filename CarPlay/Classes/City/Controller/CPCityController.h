80d81766365698138a93aaa850bfe256b000aa05
公平价 <2234475139@qq.com>
2015-07-13 16:26:27 +0800

    同城界面架构

--- CarPlay/Classes/City/Controller/CPCityController.h
+++ CarPlay/Classes/City/Controller/CPCityController.h
@@ -2,13 +2,22 @@
 //  CPCityController.h
 //  CarPlay
 //
-//  Created by 公平价 on 15/6/19.
+//  Created by 公平价 on 15/7/13.
 //  Copyright (c) 2015年 gongpingjia. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface CPCityController : UITableViewController
-- (IBAction)filterBtnClick:(id)sender;
+@interface CPCityController : UIViewController
 
 @end
+
+
+//$ git rm --cached CarPlay.xcodeproj/project.xcworkspace/xcuserdata/Alex.xcuserdatad/UserInterfaceState.xcuserstate
+//$ git commit -m "Removed the stupid strange file that shouldn't be tracked"
+//$ git push
+
+//git rm --cached CarPlay.xcodeproj/project.xcworkspace/xcuserdata/myUserName.xcuserdatad/UserInterfaceState.xcuserstate
+//git commit -m "Removed file that shouldn't be tracked"
+//
+///Users/weiyeyuan/chewan_ios
