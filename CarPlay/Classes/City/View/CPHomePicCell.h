e50d7e0ed1d4fffbbbb964c1b70e823003bd7b7c
公平价 <2234475139@qq.com>
2015-07-16 17:12:50 +0800

    City页面布局调整

---
+++ CarPlay/Classes/City/View/CPHomePicCell.h
@@ -0,0 +1,21 @@
+//
+//  CPHomePicCell.h
+//  CarPlay
+//
+//  Created by 公平价 on 15/7/15.
+//  Copyright (c) 2015年 gongpingjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class CPHomePhoto;
+
+@interface CPHomePicCell : UICollectionViewCell
+
+@property (nonatomic,strong) CPHomePhoto *homePhoto;
+
+// 获取当前cell重用标示符
++ (NSString *)identifier;
+
+
+
+@end
