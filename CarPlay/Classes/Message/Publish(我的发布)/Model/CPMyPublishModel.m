5d02ff84047ff8331bdc51aacb52bbb7b752dc0b
ZYSu <1145547694@qq.com>
2015-07-17 14:26:30 +0800

    解决pod问题

--- CarPlay/Classes/Message/Publish(我的发布)/Model/CPMyPublishModel.m
+++ CarPlay/Classes/Message/Publish(我的发布)/Model/CPMyPublishModel.m
@@ -23,17 +23,7 @@
     
     NSDate *currentDate = [NSDate dateWithTimeIntervalSince1970:start / 1000];
     NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
-    // 如果是真机调试，转换这种欧美时间，需要设置locale
-    fmt.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
     
-    // 设置日期格式（声明字符串里面每个数字和单词的含义）
-    // E:星期几
-    // M:月份
-    // d:几号(这个月的第几天)
-    // H:24小时制的小时
-    // m:分钟
-    // s:秒
-    // y:年
     fmt.dateFormat = @"MM月dd日";
     self.startStr = [fmt stringFromDate:currentDate];
 }
