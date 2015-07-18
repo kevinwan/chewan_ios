48a728077b3ecf215cca6693b72e3d0ab95e9129
ZYSu <1145547694@qq.com>
2015-07-18 16:29:41 +0800

    开始搭建创建活动界面

---
+++ CarPlay/Classes/Message/Activity(活动)/Controller/CPActivityNameController.h
@@ -0,0 +1,13 @@
+//
+//  CPActivityNameController.h
+//  CarPlay
+//
+//  Created by chewan on 15/7/18.
+//  Copyright (c) 2015年 gongpingjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPActivityNameController : UIViewController
+
+@end
---
+++ CarPlay/Classes/Message/Activity(活动)/Controller/CPEditActivityController.m
@@ -0,0 +1,38 @@
+//
+//  CPEditActivityController.m
+//  CarPlay
+//
+//  Created by chewan on 15/7/18.
+//  Copyright (c) 2015年 gongpingjia. All rights reserved.
+//  编辑活动
+
+#import "CPEditActivityController.h"
+
+@interface CPEditActivityController ()
+
+@end
+
+@implementation CPEditActivityController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"编辑活动";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
