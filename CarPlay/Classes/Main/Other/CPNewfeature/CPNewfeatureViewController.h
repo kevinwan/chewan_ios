73f0c1cf3e9166a03b6af940d7a1b9cd90e0ebac
牛鹏赛 <18032229312@163.com>
2015-07-30 15:04:11 +0800

    no message

---
+++ CarPlay/Classes/Main/Other/CPNewfeature/CPNewfeatureViewController.h
@@ -0,0 +1,13 @@
+//
+//  XMNewfeatureViewController
+//  xiaoma
+//
+//  Created by apple on 15-01-7.
+//  Copyright (c) 2014年 xiaoma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPNewfeatureViewController : UIViewController
+
+@end
---
+++ CarPlay/Classes/My/CPPhotoalbumManagement(相册管理)/CPPhotoalbumManagement.h
@@ -0,0 +1,14 @@
+//
+//  CPPhotoalbumManagement.h
+//  CarPlay
+//
+//  Created by 公平价 on 15/7/29.
+//  Copyright (c) 2015年 gongpingjia. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface CPPhotoalbumManagement : BaseViewController
+@property (weak, nonatomic) IBOutlet UIView *photoView;
+
+@end
