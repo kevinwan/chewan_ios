81f6a1f0e71d8c3bc6a8632a41ae2c3260349c16
牛鹏赛 <18032229312@163.com>
2015-08-15 11:23:47 +0800

    no message

--- CarPlay/Classes/Message/Controller/CPMessageController.m
+++ CarPlay/Classes/Message/Controller/CPMessageController.m
@@ -14,10 +14,10 @@
 #import "CPBadgeView.h"
 
 #import "ChatListCell.h"
-//#import "NSDate+Category.h"
-//#import "ChatViewController.h"
+#import "NSDate+Category.h"
+#import "ChatViewController.h"
 #import "ConvertToCommonEmoticonsHelper.h"
-//#import "RobotManager.h"
+#import "RobotManager.h"
 
 typedef enum {
     CPMessageOptionMsg, // 新留言消息
@@ -32,6 +32,10 @@ typedef enum {
 @property (weak, nonatomic) IBOutlet UILabel *activityApplyMsgLabel;
 
 @property (nonatomic, strong) NSTimer *timer;
+
+
+@property (strong, nonatomic) NSMutableArray        *dataSource;
+@property (nonatomic, strong) UIView                *networkStateView;
 @end
 
 @implementation CPMessageController
@@ -160,6 +164,155 @@ typedef enum {
         CPNewMessageController *newMsgVc = [UIStoryboard storyboardWithName:@"CPNewMessageController" bundle:nil].instantiateInitialViewController;
         [self.navigationController pushViewController:newMsgVc animated:YES];
     }
+}
+
+#pragma mark - public
+
+-(void)refreshDataSource
+{
+    self.dataSource = [self loadDataSource];
+    [self.tableView reloadData];
+//    [self hideHud];
+}
+
+- (void)isConnect:(BOOL)isConnect{
+    if (!isConnect) {
+        self.tableView.tableHeaderView = _networkStateView;
+    }
+    else{
+        self.tableView.tableHeaderView = nil;
+    }
+    
+}
+
+- (void)networkChanged:(EMConnectionState)connectionState
+{
+    if (connectionState == eEMConnectionDisconnected) {
+        self.tableView.tableHeaderView = _networkStateView;
+    }
+    else{
+        self.tableView.tableHeaderView = nil;
+    }
+}
+
+#pragma mark - private
+
+- (UIView *)networkStateView
+{
+    if (_networkStateView == nil) {
+        _networkStateView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.tableView.frame.size.width, 44)];
+        _networkStateView.backgroundColor = [UIColor colorWithRed:255 / 255.0 green:199 / 255.0 blue:199 / 255.0 alpha:0.5];
+        
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, (_networkStateView.frame.size.height - 20) / 2, 20, 20)];
+        imageView.image = [UIImage imageNamed:@"messageSendFail"];
+        [_networkStateView addSubview:imageView];
+        
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMaxX(imageView.frame) + 5, 0, _networkStateView.frame.size.width - (CGRectGetMaxX(imageView.frame) + 15), _networkStateView.frame.size.height)];
+        label.font = [UIFont systemFontOfSize:15.0];
+        label.textColor = [UIColor grayColor];
+        label.backgroundColor = [UIColor clearColor];
+        label.text = @"网络未连接";
+        [_networkStateView addSubview:label];
+    }
+    
+    return _networkStateView;
+}
+
+
+- (NSMutableArray *)loadDataSource
+{
+    NSMutableArray *ret = nil;
+    NSArray *conversations = [[EaseMob sharedInstance].chatManager conversations];
+    
+    NSArray* sorte = [conversations sortedArrayUsingComparator:
+                      ^(EMConversation *obj1, EMConversation* obj2){
+                          EMMessage *message1 = [obj1 latestMessage];
+                          EMMessage *message2 = [obj2 latestMessage];
+                          if(message1.timestamp > message2.timestamp) {
+                              return(NSComparisonResult)NSOrderedAscending;
+                          }else {
+                              return(NSComparisonResult)NSOrderedDescending;
+                          }
+                      }];
+    
+    ret = [[NSMutableArray alloc] initWithArray:sorte];
+    return ret;
+}
+
+- (void)didReceiveOfflineMessages:(NSArray *)offlineMessages
+{
+    [self refreshDataSource];
+}
+
+// 得到最后消息时间
+-(NSString *)lastMessageTimeByConversation:(EMConversation *)conversation
+{
+    NSString *ret = @"";
+    EMMessage *lastMessage = [conversation latestMessage];;
+    if (lastMessage) {
+        ret = [NSDate formattedTimeFromTimeInterval:lastMessage.timestamp];
+    }
+    
+    return ret;
+}
+
+// 得到未读消息条数
+- (NSInteger)unreadMessageCountByConversation:(EMConversation *)conversation
+{
+    NSInteger ret = 0;
+    ret = conversation.unreadMessagesCount;
+    
+    return  ret;
+}
+
+// 得到最后消息文字或者类型
+-(NSString *)subTitleMessageByConversation:(EMConversation *)conversation
+{
+    NSString *ret = @"";
+    EMMessage *lastMessage = [conversation latestMessage];
+    if (lastMessage) {
+        id<IEMMessageBody> messageBody = lastMessage.messageBodies.lastObject;
+        switch (messageBody.messageBodyType) {
+            case eMessageBodyType_Image:{
+                ret = @"图片";
+            } break;
+            case eMessageBodyType_Text:{
+                // 表情映射。
+                NSString *didReceiveText = [ConvertToCommonEmoticonsHelper
+                                            convertToSystemEmoticons:((EMTextMessageBody *)messageBody).text];
+                if ([[RobotManager sharedInstance] isRobotMenuMessage:lastMessage]) {
+                    ret = [[RobotManager sharedInstance] getRobotMenuMessageDigest:lastMessage];
+                } else {
+                    ret = didReceiveText;
+                }
+            } break;
+            case eMessageBodyType_Voice:{
+                ret = @"语音";
+            } break;
+            case eMessageBodyType_Location: {
+                ret = @"位置";
+            } break;
+            case eMessageBodyType_Video: {
+                ret = @"视频";
+            } break;
+            default: {
+            } break;
+        }
+    }
+    
+    return ret;
+}
+
+#pragma mark - ChatViewControllerDelegate
+
+// 根据环信id得到要显示头像路径，如果返回nil，则显示默认头像
+- (NSString *)avatarWithChatter:(NSString *)chatter{
+    return @"http://img0.bdstatic.com/img/image/shouye/jianbihua0525.jpg";
+}
+
+// 根据环信id得到要显示用户名，如果返回nil，则默认显示环信id
+- (NSString *)nickNameWithChatter:(NSString *)chatter{
+    return chatter;
 }
 
 @end
--- CarPlay/Classes/Message/Controller/MessageView/MessageReadManager.m
+++ CarPlay/Classes/Message/Controller/MessageView/MessageReadManager.m
@@ -11,7 +11,7 @@
   */
 
 #import "MessageReadManager.h"
-#import "UIImageView+EMWebCache.h"
+//#import "UIImageView+EMWebCache.h"
 #import "EMCDDeviceManager.h"
 
 static MessageReadManager *detailInstance = nil;
