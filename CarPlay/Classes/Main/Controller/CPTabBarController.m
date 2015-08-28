f86b0d837d2e707253a929c704354145d2c0e8ca
牛鹏赛 <18032229312@163.com>
2015-08-28 16:00:18 +0800

    no message

--- CarPlay/Classes/Main/Controller/CPTabBarController.m
+++ CarPlay/Classes/Main/Controller/CPTabBarController.m
@@ -168,7 +168,8 @@ static NSString *kGroupName = @"GroupName";
 //    UIViewController *vc = self.childViewControllers[1];
     NSString *key=[[NSString alloc]initWithFormat:@"%@unreadMessageCount",[Tools getValueFromKey:@"userId"]];
     [Tools setValueForKey:@(unreadCount) key:key];
-    if (unreadCount > 0) {
+    NSInteger zyUnreadCount=[Tools getZyUnreadMsgCount];
+    if ((zyUnreadCount + unreadCount)> 0) {
         [self.tabBar showBadgeOnItemIndex:1];
     }else{
         [self.tabBar hideBadgeOnItemIndex:1];
