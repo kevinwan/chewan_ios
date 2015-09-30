d56c86fe3f135f1f2716ba6e3155bd304b435a07
Ywy <2234475139@qq.com>
2015-09-30 10:47:59 +0800

    加载个人信息底部

--- CarPlay/My(我的)/MyInfo/Controller/CPMyInfoController.m
+++ CarPlay/My(我的)/MyInfo/Controller/CPMyInfoController.m
@@ -17,16 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.title = @"个人信息";
+    // 设置导航栏
+    [self setRightNavigationBarItemWithTitle:@"完成" Image:nil highImage:nil  target:self action:@selector(finish)];
+     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)finish{
+    
 }
 
 #pragma mark - Table view data source
@@ -37,71 +35,42 @@
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-//    if (indexPath.row == 0) {
-//        //
-//        static NSString *firId = @"myInfoFirCell";
-//        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:firId];
-//    }
-//    if (indexPath.row == 1) {
-//        //
-//        static NSString *firId = @"myInfoFirCell";
-//        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:firId];
-//    }
-//    if (indexPath.row == 2) {
-//        //
-//        static NSString *firId = @"myInfoFirCell";
-//        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:firId];
-//    }
-    
-    static NSString *firId = @"myInfoFirCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:firId];
-    
+    UITableViewCell *cell;
+    if (indexPath.row == 0) {
+        //
+        static NSString *firId = @"myInfoFirCell";
+        cell = [tableView dequeueReusableCellWithIdentifier:firId];
+        
+        if (cell == nil) {
+            cell = [[[NSBundle mainBundle] loadNibNamed:@"CPMyInfoFirCell" owner:nil options:nil] lastObject];
+        }
+        
+    }
+    if (indexPath.row == 1) {
+        //
+        static NSString *secId = @"myInfoSecCell";
+        cell = [tableView dequeueReusableCellWithIdentifier:secId];
+        
+        if (cell == nil) {
+            cell = [[[NSBundle mainBundle] loadNibNamed:@"CPMyInfoSecCell" owner:nil options:nil] lastObject];
+        }
+
+    }
+    if (indexPath.row == 2) {
+        //
+        static NSString *ThrId = @"myInfoThrCell";
+        cell = [tableView dequeueReusableCellWithIdentifier:ThrId];
+        
+        if (cell == nil) {
+            cell = [[[NSBundle mainBundle] loadNibNamed:@"CPMyInfoThrCell" owner:nil options:nil] lastObject];
+        }
+
+    }
+       
     return cell;
 }
 
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
