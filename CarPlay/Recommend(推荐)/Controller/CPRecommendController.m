130740a4142811fffe81bb833364c7b165484e22
ZYSu <1145547694@qq.com>
2015-10-09 11:33:54 +0800

    测试

--- CarPlay/Recommend(推荐)/Controller/CPRecommendController.m
+++ CarPlay/Recommend(推荐)/Controller/CPRecommendController.m
@@ -32,11 +32,24 @@
 {
     if (_collectionView == nil) {
         UICollectionViewFlowLayout *layout = [UICollectionViewFlowLayout new];
-        layout.minimumLineSpacing = 40;
-        layout.itemSize = CGSizeMake(ZYScreenWidth - 50, 405 + ZYScreenWidth - 320);
+        layout.minimumLineSpacing = 0;
+        layout.minimumInteritemSpacing = 0;
+        layout.sectionInset = UIEdgeInsetsZero;
+        layout.itemSize = CGSizeMake(ZYScreenWidth - 40, 405 + ZYScreenWidth - 320);
         ;
-//        layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
-        _collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];;
+        layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+        
+        _collectionView = [[UICollectionView alloc] initWithFrame:({
+            CGFloat x = 40;
+            CGFloat y = 84;
+            CGFloat w = ZYScreenWidth - 80;
+            CGFloat h = ZYScreenHeight - 64 - 49 - 48;
+            CGRect frame = CGRectMake(x, y, w, h);
+            frame;
+        }) collectionViewLayout:layout];
+        _collectionView.clipsToBounds = NO;
+//        _collectionView.contentInset = UIEdgeInsetsMake(0, 30, 0, 0);
+        _collectionView.pagingEnabled = YES;
         _collectionView.delegate = self;
         _collectionView.dataSource = self;
         [_collectionView registerNib:[UINib nibWithNibName:@"CPRecommendCell" bundle:nil] forCellWithReuseIdentifier:@"item"];
