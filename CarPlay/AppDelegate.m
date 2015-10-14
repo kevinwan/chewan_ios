fda5619f748f85c47d8f7f014e048ffd33f453ff
jiang <1163637383@qq.com>
2015-10-14 18:09:36 +0800

    聊天图片（环信部分），登陆操作（test）

--- CarPlay/AppDelegate.m
+++ CarPlay/AppDelegate.m
@@ -53,7 +53,17 @@
         // 3.开始监听(开始获取位置)
         [self.mgr startUpdatingLocation];
     }
+    //环信
+    //gongpingjia#carplayapp
+    [[EaseMob sharedInstance] registerSDKWithAppKey:@"easemob-demo#chatdemoui" apnsCertName:@"istore_dev"];
+    [[EaseMob sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
     
+    [[EaseMob sharedInstance].chatManager asyncLoginWithUsername:@"offen3" password:@"123" completion:^(NSDictionary *loginInfo, EMError *error) {
+        if (!error) {
+            // 设置自动登录
+            [[EaseMob sharedInstance].chatManager setIsAutoLoginEnabled:YES];
+        }
+    } onQueue:nil];
     return YES;
 }
 
@@ -103,12 +113,11 @@
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+[[EaseMob sharedInstance] applicationDidEnterBackground:application];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+[[EaseMob sharedInstance] applicationWillEnterForeground:application];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
@@ -116,7 +125,7 @@
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+ [[EaseMob sharedInstance] applicationWillTerminate:application];
 }
 
 -(void)loginStateChang{
