7f19f46af25240e010ee71a60f35467c2db43838
牛鹏赛 <18032229312@163.com>
2015-10-19 21:15:14 +0800

    no message

--- CarPlay/Matching(匹配)/View/OtherMatchingSelectView.h
+++ CarPlay/Matching(匹配)/View/OtherMatchingSelectView.h
@@ -1,43 +1,13 @@
 //
-//  MatchingSelectView.h
+//  OtherMatchingSelectView.h
 //  CarPlay
 //
-//  Created by 公平价 on 15/10/16.
+//  Created by 公平价 on 15/10/19.
 //  Copyright © 2015年 chewan. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import "MJNIndexView.h"
 
-@interface OtherMatchingSelectView : UIView<UITableViewDataSource,UITableViewDelegate,MJNIndexViewDataSource>
-@property (weak, nonatomic) IBOutlet UIView *selectView;
-@property (weak, nonatomic) IBOutlet UIButton *selectPlace;
-- (IBAction)shuttle:(id)sender;
-@property (weak, nonatomic) IBOutlet UIButton *matchingBtn;
-- (IBAction)selectPlaceClick:(id)sender;
-@property (weak, nonatomic) IBOutlet UIButton *shuttleBtn;
-//1包接送  0不包接送
-@property (nonatomic, strong) NSString *whetherShuttle;
-- (IBAction)matchingBtnClick:(id)sender;
-+(void)show:(NSString *)colorStr;
-@property (weak, nonatomic) IBOutlet UIView *locationAddressView;
+@interface OtherMatchingSelectView : UIViewController
 
-@property (weak, nonatomic) IBOutlet UIView *addressSelection;
-- (IBAction)closeLocatoinAddressView:(id)sender;
-@property (weak, nonatomic) IBOutlet UILabel *addressLable;
-@property (weak, nonatomic) IBOutlet UIButton *confirmButton;
-@property (weak, nonatomic) IBOutlet UIButton *reSelectionButton;
-- (IBAction)confirm:(id)sender;
-- (IBAction)reSelection:(id)sender;
-
-- (IBAction)closeAddressSelectionView:(id)sender;
-@property (weak, nonatomic) IBOutlet UILabel *locationAddressLable;
-@property (weak, nonatomic) IBOutlet UITableView *addressTableView;
-@property (nonatomic, assign) NSInteger parentId;
-//排序后
-@property (nonatomic, strong) NSMutableArray *areaList;
-//排序前
-@property (nonatomic, strong) NSArray *areaListBeforeSort;
-
-@property (nonatomic, strong) MJNIndexView *indexView;
 @end
