383260844135738c77323119b4cfb7f3f9d0e7f9
牛鹏赛 <18032229312@163.com>
2015-10-23 12:38:46 +0800

    no message

---
+++ CarPlay/Common(公用)/Model/CPCar.h
@@ -0,0 +1,13 @@
+//
+//  CPCar.h
+//  CarPlay
+//
+//  Created by 公平价 on 15/10/23.
+//  Copyright © 2015年 chewan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPCar : NSObject
+
+@end
