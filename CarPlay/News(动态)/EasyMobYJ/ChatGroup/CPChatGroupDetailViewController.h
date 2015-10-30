55e7b2a79e63d745c048f3e221c63e6d30b1cbd1
jiang <1163637383@qq.com>
2015-10-30 13:19:09 +0800

    群聊详情页面

---
+++ CarPlay/News(动态)/EasyMobYJ/ChatGroup/CPChatGroupDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPChatGroupDetailViewController.h
+//  CarPlay
+//
+//  Created by jiang on 15/10/30.
+//  Copyright © 2015年 chewan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPChatGroupDetailViewController : UIViewController
+
+@end
---
+++ CarPlay/News(动态)/EasyMobYJ/ChatGroup/CPGroupDetailTBCell.m
@@ -0,0 +1,23 @@
+//
+//  CPGroupDetailTBCell.m
+//  CarPlay
+//
+//  Created by jiang on 15/10/30.
+//  Copyright © 2015年 chewan. All rights reserved.
+//
+
+#import "CPGroupDetailTBCell.h"
+
+@implementation CPGroupDetailTBCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
