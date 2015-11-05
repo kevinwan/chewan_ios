b0475a151ad33a0c68c2cbf3cea62792d80e7c3d
ZYSu <1145547694@qq.com>
2015-11-05 21:10:55 +0800

    修复车标bug

--- CarPlay/Near(附近)/View/CPBaseViewCell.m
+++ CarPlay/Near(附近)/View/CPBaseViewCell.m
@@ -291,6 +291,7 @@
         self.carTypeView.hidden = NO;
         
         [self.carView zySetImageWithUrl:model.organizer.car.logo placeholderImage:[UIImage imageNamed:@"车主未认证"] forState:UIControlStateNormal];
+        self.carTypeView.hidden = NO;
         self.carTypeView.text = model.organizer.car.model;
     }else{
         
@@ -373,10 +374,10 @@
     }else{
         [self.authView setImage:[UIImage imageNamed:@"未认证-审核中"] forState:UIControlStateNormal];
     }
-    
     if ([myDateModel.applicant.licenseAuthStatus isEqualToString:@"认证通过"]) {
         [self.carView zySetImageWithUrl:myDateModel.applicant.car.logo placeholderImage:[UIImage imageNamed:@"车主未认证"] forState:UIControlStateNormal];
-        self.carTypeView.text = myDateModel.applicant.car.model;
+         self.carTypeView.hidden = NO;
+         self.carTypeView.text = myDateModel.applicant.car.model;
     }else{
         [self.carView setImage:[UIImage imageNamed:@"车主未认证"] forState:UIControlStateNormal];
         self.carTypeView.hidden = YES;
@@ -477,7 +478,7 @@
     if ([intersterModel.user.licenseAuthStatus isEqualToString:@"认证通过"]) {
         self.carTypeView.hidden = NO;
         [self.carView zySetImageWithUrl:intersterModel.user.car.logo placeholderImage:[UIImage imageNamed:@"车主未认证"] forState:UIControlStateNormal];
-        
+        self.carTypeView.hidden = NO;
         self.carTypeView.text = intersterModel.user.car.model;
     }else{
         [self.carView setImage:[UIImage imageNamed:@"车主未认证"] forState:UIControlStateNormal];
